3a2d13a2a003a7da452fad89e9344cda6a7bf4a7
crocodeev <svin862003@mail.ru>
2022-11-03 14:51:36 +0300

    inplay telegram control

--- frontend/companents/Bar.jsx
+++ frontend/companents/Bar.jsx
@@ -2,22 +2,18 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { useState } from 'react';
-
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const Bar = () => {
  
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl" color="green">
+    <AppBar position="static"
+      color="success">
+      <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Typography
             variant="h5"
             noWrap
             component="a"
-            href=""
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },
